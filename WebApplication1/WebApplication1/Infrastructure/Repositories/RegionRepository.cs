37de329e06ed6d79468ad1139d14429d1f24bb04
mirace-work <kiennguyen16998.com>
2024-10-10 17:38:24 +0700

    update native query

--- WebApplication1/WebApplication1/Infrastructure/Repositories/RegionRepository.cs
+++ WebApplication1/WebApplication1/Infrastructure/Repositories/RegionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebApplication1.Core.Entities;
+using WebApplication1.DTOs;
 using WebApplication1.Infrastructure.Data;
 
 namespace WebApplication1.Infrastructure.Repositories
@@ -42,6 +43,33 @@ namespace WebApplication1.Infrastructure.Repositories
         public Task UpdateAsync(Region entity)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<int> ExecuteCustomQueryAsync()
+        {
+            string sqlQuery = "UPDATE Regions SET Name = 'UpdatedName' WHERE Id = {0}";
+            return await _context.Database.ExecuteSqlRawAsync(sqlQuery, 1); // 1 là ID của vùng
+        }
+
+        public async Task<IEnumerable<Region>> GetRegionsByNativeQueryAsync(string condition)
+        {
+            string sqlQuery = "SELECT * FROM Regions WHERE Name LIKE {0}";
+            return await _context.Regions.FromSqlRaw(sqlQuery, $"%{condition}%").ToListAsync();
+        }
+
+        public async Task<IEnumerable<RegionDto>> GetRegionsNameByNativeQueryAsync(string condition)
+        {
+            string sqlQuery = "SELECT Id, Name FROM Regions WHERE Name LIKE {0}"; // Chỉ lấy Id và Name
+            var regions = await _context.Regions
+                .FromSqlRaw(sqlQuery, $"%{condition}%")
+                .Select(r => new RegionDto
+                {
+                    Id = r.Id, // Gán Id từ lớp Region
+                    Name = r.Name // Gán Name từ lớp Region
+                })
+                .ToListAsync();
+
+            return regions;
         }
     }
 }
--- WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251160ed87c68659f51cce5ed66466b5062e72b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
